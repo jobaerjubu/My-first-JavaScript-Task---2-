5a61549980faf06ddd6763fa8f5ca226d1b80c10
jobaerjubu <jobaerahmed4142@gmail.com>
2025-09-04 10:56:42 +0600

    added a GradeCalculator

---
+++ GradeCalculator.js
@@ -0,0 +1,51 @@
+/***
+
+Grade Calculator
+
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
+
+
+var score = 99;
+
+var Grade;
+
+
+if (score >= 90 &&  score <=  100  ) {
+
+    Grade = 'A' ;
+}
+
+else if ( score >= 80 &&  score <=  89 ) {
+
+     Grade = 'B' ;
+
+} 
+
+else if ( score >= 70 &&  score <=  79 ) {
+
+     Grade = 'C' ;
+
+}
+else if ( score >= 60 &&  score <=  69 ) {
+
+     Grade = 'D' ;
+
+}
+
+
+else if ( score >= 0 &&  score <=  59 ) {
+
+     Grade = 'F' ;
+
+}
+
+
+console.log('Your Grade is:' , Grade );
